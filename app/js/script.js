80b23b6063cacb4ca6add32a135f188fe9638d10
alexeymasnoy <masnoy0alexey@gmail.com>
2015-05-17 19:00:22 +0300

    добавлены изменения

--- app/js/script.js
+++ app/js/script.js
@@ -4,18 +4,6 @@ $(document).ready(function(){
         
     }, function (){
     	$(this).children("ul").stop().fadeOut(300);
-    });	
-    
-    $(".pictures img").mouseover( function(){ 
-        $(this).parent().parent().css('background','#2f2e2e');
-        $(this).animate({opacity: "0.3"}, "1");
- 
-    });   
-
-    $(".pictures img").mouseout( function(){ 
-        $(this).parent().parent().css('background','none'); 
-        $(this).animate({opacity: "1"}, "1");                                                       
-     
-    });           
+    });	          
     
 });
